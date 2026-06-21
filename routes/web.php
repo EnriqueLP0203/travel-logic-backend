471233dfa09fcb39bf3fa329d5d85761f6fd14a1
EnriqueLP0203 <2302045@utrivieramaya.edu.mx>
2026-06-21 13:28:53 -0500

    add routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/quoter', function () {
+    return view('quoter');
+})->name('quoter');
+
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
