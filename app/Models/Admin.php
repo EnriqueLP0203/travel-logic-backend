22aa582cca5b850d162e3f60fca5b5d664ffed50
EnriqueLP0203 <2302045@utrivieramaya.edu.mx>
2026-07-17 15:16:24 -0500

    Add Admin model and migration to rename users table to admins with username field

---
+++ app/Models/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+#[Fillable(['name', 'username', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = 'admins';
+
+    /**
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
